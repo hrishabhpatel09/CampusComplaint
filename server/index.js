e6268ddda0f38da5d00a6ca41f62c76c26797117
Hrishabh Patel <hrishabhpatel2023@gmail.com>
2026-04-22 21:21:09 +0530

    resolved cors errors

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const { initDb } = require('./db')
 
 const app = express()
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
+app.use(cors({ origin: '*', credentials: true }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
