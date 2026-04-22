c1fdb987070fd9324d3b3a3c627e34634de66680
Hrishabh Patel <hrishabhpatel2023@gmail.com>
2026-04-22 21:19:00 +0530

    added Env Variables

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: 'http://localhost:8000/api' })
+const baseURL = import.meta.env.VITE_SERVER_URL;
+const api = axios.create({ baseURL: baseURL })
+
 
 // Attach JWT to every request
 api.interceptors.request.use(cfg => {
